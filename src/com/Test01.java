c02879a8d2bbf9fd53efa839a84548a120da8455
lijinxiang1111 <1946577676@qq.com>
2021-09-24 12:27:08 +0800

    first commit

--- src/com/Test01.java
+++ src/com/Test01.java
@@ -2,15 +2,10 @@ package com;
 
 public class Test01 {
     public static void main(String[] args) {
-        byte b1 = 1, b2 = 2, b3, b6;
+        byte b6;
         final byte b4 = 4, b5 = 6;
         b6 = b4 + b5;
-        /*b3 = (b1 + b2);*/
         System.out.println(b6);
-
-
-        /*String s;
-        System.out.println("s=" + s);*/
     }
 }
 
